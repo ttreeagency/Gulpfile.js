9d0e2e811603362973858b83dba81b1d1dc440a2
Dominique Feyer <dfeyer@ttree.ch>
2017-07-15 21:28:31 +0200

    TASK: Update styleguide

--- tasks/css.js
+++ tasks/css.js
@@ -34,6 +34,7 @@ let postScss = [
 	require('postcss-selector-matches'),
 	require('postcss-selector-not'),
 	require('postcss-pseudoelements')(pc.pseudoelements),
+	require('postcss-font-awesome'),
 	require('postcss-custom-media'),
 	require('postcss-media-minmax'),
 	require('postcss-quantity-queries'),
