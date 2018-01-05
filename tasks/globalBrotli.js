8da356a526fd2a8ba11039a1cdc178365b58e87a
Dominique Feyer <dfeyer@ttree.ch>
2018-01-05 15:46:28 +0100

    BUGFIX: Compress CSS with Brotli

--- tasks/globalBrotli.js
+++ tasks/globalBrotli.js
@@ -7,7 +7,7 @@ if (!config.tasks.compress) {
 function compress() {
   return gulp.src([
     'Packages/*/*/Resources/Public/**/*.js',
-    'Packages/*/*/Resources/Public/**/*.js'
+    'Packages/*/*/Resources/Public/**/*.css'
   ])
     .pipe(brotli.compress())
     .pipe(gulp.dest(function(file) {
